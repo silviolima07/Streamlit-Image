bf38608d75a289d96378c52b1922f61b6242b4bc
silviolima07 <silviolima07@gmail.com>
2022-01-17 20:24:34 -0300

    First commit

---
+++ app2.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import os
+
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Selecione o dataset para avaliação', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+filename = file_selector()
+st.write('Dataset`%s`' % filename)
+
